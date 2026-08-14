c41c89134fc09f6b05275d807ffad577999d77b2
sadikmathakiya07-glitch <sadikmathakiya07@gmail.com>
2026-08-14 11:16:36 +0530

    chnages

--- ReactJs-Lab/React-Lab/src/App.jsx
+++ ReactJs-Lab/React-Lab/src/App.jsx
@@ -2,14 +2,20 @@ import P1 from "./Lab-14/P1"
 import P2 from "./Lab-14/P2"
 import WithClass from "./Lab-15/WithClass"
 import WithFunct from "./Lab-15/WithFunct"
+import Faculties from "./Lab-17/Faculties"
+import MapMethod from "./Lab-17/MapMethod"
+import Student from "./Lab-17/Student"
 
 function App() {
   return (
     <>
       {/* <P1/> */}
       {/* <P2/> */}
-      <WithClass/>
-      <WithFunct/>
+      {/* <WithClass/> */}
+      {/* <WithFunct/> */}
+      <MapMethod/>
+      <Faculties/>
+      <Student/>
     </>
   )
 }
